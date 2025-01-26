0ee707c33b4eb9379e8035808338757696072fea
paek1127 <paek1127@gmail.com>
2025-01-26 16:17:07 +0900

    chore: security 설정

---
+++ src/main/java/com/ll/security_2025_01_06/global/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ll.security_2025_01_06.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain baseSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests // 시큐리티는 상위 우선법이라 룰이 충돌할 경우 상위 룰이 우선시 된다.
+                                .requestMatchers("/h2-console/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts", "/api/*/posts/{postId:\\d+}/comments")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .headers(headers ->
+                        headers.frameOptions(frameOptions ->
+                                frameOptions.sameOrigin())
+                )
+                .csrf(csrf ->
+                        csrf.disable()
+                );
+
+        return http.build();
+    }
+}
