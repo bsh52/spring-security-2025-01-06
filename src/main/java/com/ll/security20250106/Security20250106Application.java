edd2bd032ab35a96552c6eb487da4e4d9d011573
paek1127 <paek1127@gmail.com>
2025-01-26 14:25:22 +0900

    base

---
+++ src/main/java/com/ll/security20250106/Security20250106Application.java
@@ -0,0 +1,13 @@
+package com.ll.security20250106;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security20250106Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security20250106Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/security20250106/Security20250106ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.security20250106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security20250106ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
